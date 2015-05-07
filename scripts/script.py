c4db304f94e410e17b1bf70f8c9fe14c8558213c
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-05-06 22:44:23 -0700

    ENH: Add pick otus closed ref

--- scripts/script.py
+++ scripts/script.py
@@ -11,6 +11,16 @@ from qcli import qcli_system_call
 from skbio.util import create_dir
 
 
+def system_call(cmd, shell=True):
+    """Simple wrapper around qcli_system_call"""
+    info = qcli_system_call(cmd)
+
+    if info[2] != 0:
+        logging.error('STDOUT: '+info[0])
+        logging.error('STDERR: '+info[1])
+        return sys.exit(info[2])
+
+
 def main():
 
     # load json file
@@ -31,26 +41,32 @@ def main():
 
     # from BaseSpace's documentation
     input_dir = '/data/input/samples/'
-    output_dir = join('/data/output/appresults/', project_id, 'sl_out')
+    base = join('/data/output/appresults/', project_id)
+    output_dir = join(base, 'sl-out')
 
     # for sanity
     create_dir(output_dir)
 
+    # split libraries
     cmd = ("multiple_split_libraries_fastq.py "
            "-i '{input_dir}' -o '{output_dir}'")
     params = {'input_dir': input_dir, 'output_dir': output_dir}
 
-    info = qcli_system_call(cmd.format(**params))
-
-    if info[2] != 0:
-        logging.error('STDOUT: '+info[0])
-        logging.error('STDERR: '+info[1])
-        return info[2]
-
+    system_call(cmd.format(**params))
     for log_file in glob(join(output_dir, 'log_*')):
         with open(log_file, 'U') as fd_log:
             print fd_log.read()
 
+    input_dir = join(output_dir, 'seqs.fna')
+    output_dir = join(base, 'closed-ref')
+    cmd = ("pick_closed_reference_otus.py "
+           "-i '{input_seqs}' -o '{output_dir}'")
+    params = {'input_seqs': input_dir, 'output_dir': output_dir}
+
+    system_call(cmd.format(**params))
+    for log_file in glob(join(output_dir, 'log_*')):
+        with open(log_file, 'U') as fd_log:
+            print fd_log.read()
 
 if __name__ == '__main__':
     sys.exit(main())
