d77083c8fa662479c5235b912497d0cf3296538a
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-05-26 23:49:51 -0700

    MAINT: Finalize Upstream App

--- basespace/upstream/callback.js
+++ basespace/upstream/callback.js
@@ -3,7 +3,7 @@ function launchSpec(dataProvider)
     var ret = {
         // see script.sh in this folder 
         appSessionName: "bsq pre-alpha",
-        commandLine: [ "bash", "/script.sh" ],
+        commandLine: [ "bash", "/upstream.sh" ],
         containerImageId: "yoshikiv/bsq-debugging"
     };
     return ret;
