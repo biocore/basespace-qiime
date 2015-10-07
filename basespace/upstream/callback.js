4aa7390550bb895e0a5fb3b6399379613fb14fc1
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-10-07 10:32:12 -0700

    ENH: Update files

--- basespace/upstream/callback.js
+++ basespace/upstream/callback.js
@@ -4,9 +4,8 @@ function launchSpec(dataProvider)
         // see script.sh in this folder 
         appSessionName: "basespace-qiime pre-alpha",
         commandLine: [ "bash", "/upstream.sh" ],
-        containerImageId: "yoshikiv/basespace-qiime-191",
+        containerImageId: "yoshikiv/basespace-qiime-191-dev@sha256:470b4ae8960a28309fdeabe06caea6728cfe6d70cbce7c7e3c5d2dd017927cc6",
         Options: [ "bsfs.enabled=true" ]
     };
     return ret;
 }
-
